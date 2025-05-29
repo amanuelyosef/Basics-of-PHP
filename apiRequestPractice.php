f8fa672cc6f433f1d5d2b8b4ff0e64c0f2a462ee
aman <amanuyosi12@gmail.com>
2025-05-29 09:46:31 +0300

    practice api request 2

--- apiRequestPractice.php
+++ apiRequestPractice.php
@@ -5,18 +5,21 @@ $data = ["message"=>"sent", "with"=>"greetfully"];
 
 $ch = curl_init($url);
 
-curl_setopt($ch, CURLOPT_RETURNTRANSFERS, true);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//curl_setopt($ch, CURLOPT_POST, true);
+//curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
 curl_setopt($ch, CURLOPT_HTTPHEADER, ["Content-Type: application/json"]);
 
 $respons = curl_exec($ch);
 
 if(curl_errno($ch)){
-    die("error number ". curl_errno($ch));
+    die("error number ". curl_errno($ch) . "<br>" . curl_error($ch));
 }
 
 curl_close($ch);
 
+$dataArray = json_decode($respons, true);
+echo "Hello<br>";
+print_r($dataArray);
 
 ?>
